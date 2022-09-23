23fefe0684826a8f3db855d3f4cfdc2dbab20ff7
Unai Mató <77381425+unaiiM@users.noreply.github.com>
2022-09-23 23:49:25 +0200

    Add files via upload

--- api/tests/test.js
+++ api/tests/test.js
@@ -1,12 +1,22 @@
 const lib = "./../lib/";
 const Downloader = require(lib + "yt-downloader.js");
 
-const url = "https://www.youtube.com/watch?v=QTXwz57eFQI";
-let download = new Downloader(url);
-const options = { video : { quality : 720, audio : true }};
-async function abc(){
-	await download.load();
-	//console.log(download.formats);
-	await download.download(options);
-};
-abc();
+const url = "https://www.youtube.com/watch?v=jUoHX7i03nY";
+const download = new Downloader(url);
+const options = { video : { quality : 1080 }, audio : { quality : "medium" }};
+
+download.load();
+
+download.on("load", () => {
+
+	download.download(options);
+
+});
+
+download.on("error", (err) => { if(err) throw err; })
+
+download.on("download-finished", () => {
+
+	// somthing	
+
+});
